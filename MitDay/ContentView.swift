924d79cec946bc868b5a35950074ab5ffabff323
WelingtonMelo <welington.melo@b23s.com.br>
2025-09-03 20:19:35 -0300

    Initial Commit

---
+++ MitDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MitDay
+//
+//  Created by WMELO on 03/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
